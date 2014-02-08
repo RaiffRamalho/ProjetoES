1def3177a116104d3330ddfee1b174feb0fe559d
unknown <eduardocme@gmail.com>
2014-02-08 18:29:02 -0200

    Progress Bar

--- src/com/example/projetoes/Jogo.java
+++ src/com/example/projetoes/Jogo.java
@@ -3,18 +3,45 @@ package com.example.projetoes;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class Jogo extends Activity {
+	
+	private static final int PROGRESS = 0x1;
+
+    private int mProgressStatus = 0;
+    
+    private Handler mHandler = new Handler();	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.jogo);
-	
+		
+		final ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
+		
+		 new Thread(new Runnable() {
+            public void run() {
+                while (mProgressStatus < 3600) {
+               	 try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            bar.incrementProgressBy(1);;
+                        }
+                    });
+                }
+         
+            }
+        }).start();
 		
 		final Button buttonBack = (Button) findViewById(R.id.buttonBack);
         buttonBack.setOnClickListener(new View.OnClickListener() {
